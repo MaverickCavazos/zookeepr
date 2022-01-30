2f02f0f4ee815427f98a76f2d32012569795a8f9
Maverick <Mav33987@gmail.com>
2022-01-30 01:17:44 -0600

    fixed some bugs'

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended:true }));
-// parse incoming JSON data
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -68,7 +67,6 @@ function validateAnimal(animal) {
   return true;
 }
 
-
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -90,15 +88,13 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
-    const animal = creatNewAnimal(req.body, animals);
+    const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
 });
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
